5b046f19945b8371d83eb0507616f7cc5c6d4764
basit <basitabdullah1122@gmail.com>
2025-05-11 21:26:29 +0530

    Fix

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://linux-chat-production.up.railway.app",
+  baseURL: "https://linux-chat-production.up.railway.app/api",
   withCredentials: true,
 });
