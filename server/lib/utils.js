e4137f6056ad273c501f3cd9fba267099a0e3148
Basit <basitabdullah1122@gmail.com>
2025-05-12 10:11:37 +0530

    latest

--- server/lib/utils.js
+++ server/lib/utils.js
@@ -7,9 +7,9 @@ export const generateToken = (userId, res) => {
 
   res.cookie("jwt", token, {
     maxAge: 1000 * 60 * 60 * 24 * 7,
-    httpOnly: false, // ❗️ Less secure (accessible via JavaScript)
-    sameSite: "Lax", // ✅ Works more broadly across devices
-    secure: false,
+    httpOnly: true, 
+    sameSite: "None",
+    secure: true,
   });
 
   return token;
